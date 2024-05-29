26d5ebe266ef0dc5e42444e4afccb329ede97c01
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-05-29 15:18:12 +0900

    Form, Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import Circle from './Circle';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+const Dummy = ({ text, active = false }: DummyProps) => {
+  return <H1>{text}</H1>;
+};
 
 function App() {
   return (
-    <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </div>
+    <Container>
+      <H1>hello</H1>
+      <Dummy active={true} text="asd"></Dummy>
+    </Container>
   );
 }
 
