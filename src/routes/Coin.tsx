5480be886d382616860da88545463431ce77c7f9
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-05-29 16:53:47 +0900

    Styles

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+export default function Coin() {
+  const { coinId } = useParams();
+
+  return <h1>Coin : {coinId}</h1>;
+}
