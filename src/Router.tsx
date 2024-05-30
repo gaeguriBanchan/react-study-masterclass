7980535181b57a1b91079dab12073505b7e587b0
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-05-30 17:33:37 +0900

    Nested Routes part1

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Coins from './routes/Coins';
 import Coin from './routes/Coin';
+import Chart from './routes/Chart';
+import Price from './routes/Price';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Coins />}></Route>
-        <Route path="/:coinId" element={<Coin />}></Route>
+        <Route path="/:coinId" element={<Coin />}>
+          <Route path="chart" element={<Chart />} />
+          <Route path="price" element={<Price />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import {
+  Outlet,
+  Route,
+  Routes,
+  useLocation,
+  useParams,
+} from 'react-router-dom';
 import { json } from 'stream/consumers';
 import styled from 'styled-components';
+import Price from './Price';
+import Chart from './Chart';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -19,6 +27,28 @@ const Header = styled.header`
 const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
 `;
 
 const Loader = styled.span`
@@ -98,22 +128,56 @@ export default function Coin() {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
-      console.log(infoData);
+      // console.log(infoData);
       const priceDate = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-      console.log(priceDate);
+      // console.log(priceDate);
       setInfo(infoData);
       setPriceInfo(priceDate);
+      setLoading(false);
     })();
   }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || 'Loading...'}</Title>
+        <Title>
+          {state?.name ? state.name : loading ? 'Loading...' : info?.name}
+        </Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>{info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Open Source:</span>
+              <span>{info?.open_source ? 'Yes' : 'No'}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Outlet />
+        </>
+      )}
     </Container>
   );
 }
