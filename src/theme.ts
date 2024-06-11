2c1e1e6450281007f230dea393d98e0f80ff66ff
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-06-11 17:48:37 +0900

    styles and placeholder

--- src/theme.ts
+++ src/theme.ts
@@ -1,15 +1,11 @@
 import { DefaultTheme } from 'styled-components/dist/types';
 
 export const darkTheme: DefaultTheme = {
-  bgColor: '#2f3640',
-  textColor: 'white',
-  accentColor: '#9c88ff',
-  cardBgColor: 'transparent',
+  bgColor: '#3F8CF2',
+  boardColor: '#DADFE9',
+  cardColor: 'white',
 };
 
-export const lightTheme: DefaultTheme = {
-  bgColor: 'whitesmoke',
-  textColor: 'black',
-  accentColor: '#9c88ff',
-  cardBgColor: 'white',
-};
+// export const lightTheme: DefaultTheme = {
+//   bgColor: 'whitesmoke',
+// };
