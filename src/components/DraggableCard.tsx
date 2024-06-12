fab357af11369510b41f79142a1887fd32264538
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-06-12 11:42:40 +0900

    React.memo

---
+++ src/components/DraggableCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+interface IDraggableCardProps {
+  toDo: string;
+  index: number;
+}
+
+function DraggableCard({ toDo, index }: IDraggableCardProps) {
+  console.log(toDo);
+  return (
+    <>
+      <Draggable key={toDo} draggableId={toDo} index={index}>
+        {(magic) => (
+          <Card
+            ref={magic.innerRef}
+            {...magic.dragHandleProps}
+            {...magic.draggableProps}
+          >
+            {toDo}
+          </Card>
+        )}
+      </Draggable>
+    </>
+  );
+}
+// React.memo : 전체가 리렌던링 되는것을 막아준다. props를 바꾸면 그 item들만 다시 렌더링함
+export default React.memo(DraggableCard);
