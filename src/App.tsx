f326bb3965eb8442825b1c48c5f757ff9fb5fa1c
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-06-20 17:18:17 +0900

    Drag Part2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
@@ -20,25 +20,40 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants = {
-  hover: { scale: 1.5, rotate: 90 },
   tap: { scale: 1, borderRadius: '100px' },
-  drag: {
-    backgroundColor: 'rgb(46,204,113)',
-    transition: { duration: 1 },
-  },
 };
 
+const BiggerBox = styled(motion.div)`
+  width: 600px;
+  height: 600px;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  /* overflow: hidden; */
+`;
+
 function App() {
+  // ref : 코드로 특정 Element를 잡을 수 있는 방법
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <>
       <Wrapper>
-        <Box
-          drag
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="tap"
-          whileDrag="drag"
-        ></Box>
+        <BiggerBox ref={biggerBoxRef}>
+          <Box
+            drag
+            // dragConstraints drag 가능 범위
+            dragConstraints={biggerBoxRef}
+            // dragSnapToOrigin: drag 종료되면 원위치
+            dragSnapToOrigin
+            // dragElastic: drag 가능범위를 넘어갈때 탄성 조절, 0~1, 기본은 0.5
+            dragElastic={0.5}
+            variants={boxVariants}
+            whileHover="hover"
+            whileTap="tap"
+          ></Box>
+        </BiggerBox>
       </Wrapper>
     </>
   );
