4cb013e4ced2180b5167c3592f35de3ca28d48b4
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-06-20 16:07:38 +0900

    Gestures

--- src/App.tsx
+++ src/App.tsx
@@ -14,55 +14,21 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  background-color: white;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  width: 70px;
-  height: 70px;
-  place-self: center;
-  border-radius: 35px;
-`;
-
 const boxVariants = {
-  hidden: { opacity: 0, scale: 0.5 },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      type: 'spring',
-      duration: 1.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const circleVariants = {
-  hidden: { y: 20, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-  },
+  hover: { scale: 1.5, rotate: 90 },
+  tap: { scale: 1, borderRadius: '100px' },
 };
 
 function App() {
   return (
     <>
       <Wrapper>
-        <Box variants={boxVariants} initial="hidden" animate="visible">
-          <Circle variants={circleVariants} />
-          <Circle variants={circleVariants} />
-          <Circle variants={circleVariants} />
-          <Circle variants={circleVariants} />
-        </Box>
+        <Box variants={boxVariants} whileHover="hover" whileTap="tap"></Box>
       </Wrapper>
     </>
   );
