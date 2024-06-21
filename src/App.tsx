b46405da556833aab0cfe5ab860728b1f8d60005
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-06-21 14:55:39 +0900

    layoutId

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,9 @@ const Wrapper = styled(motion.div)`
   width: 100vw;
   height: 100vh;
   margin: 0 auto;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
-  /* flex-direction: column; */
 `;
 
 const Box = styled(motion.div)`
@@ -20,8 +19,8 @@ const Box = styled(motion.div)`
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: flex;
-  /* justify-content: center;
-  align-items: center; */
+  justify-content: center;
+  align-items: center;
   font-size: 24px;
 `;
 
@@ -29,7 +28,6 @@ const Circle = styled(motion.div)`
   background-color: #00a5ff;
   width: 100px;
   height: 100px;
-  border-radius: 50px;
 `;
 
 function App() {
@@ -40,13 +38,23 @@ function App() {
   return (
     <>
       <Wrapper onClick={ontoggleBtn}>
-        <Box
-          style={{
-            justifyContent: clicked ? 'center' : 'start',
-            alignItems: clicked ? 'center' : 'start',
-          }}
-        >
-          <Circle layout />
+        <Box>
+          {!clicked ? (
+            <Circle
+              layoutId="circle"
+              style={{ borderRadius: 50 }}
+              // transition={{ duration: 3 }}
+            />
+          ) : null}
+        </Box>
+        <Box>
+          {clicked ? (
+            <Circle
+              layoutId="circle"
+              style={{ borderRadius: 0, scale: 2 }}
+              // transition={{ duration: 3 }}
+            />
+          ) : null}
         </Box>
       </Wrapper>
     </>
