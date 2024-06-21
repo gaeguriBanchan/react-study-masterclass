daa77cbe29d3fee1ff1a26b71f2116f2a9994e5f
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-06-21 15:52:18 +0900

    layoutId Part2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Wrapper = styled(motion.div)`
   display: flex;
@@ -13,8 +13,7 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
@@ -24,10 +23,24 @@ const Box = styled(motion.div)`
   font-size: 24px;
 `;
 
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  width: 100px;
-  height: 100px;
+const Grid = styled.div`
+  width: 50vw;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
@@ -38,24 +51,23 @@ function App() {
   return (
     <>
       <Wrapper onClick={ontoggleBtn}>
-        <Box>
-          {!clicked ? (
-            <Circle
-              layoutId="circle"
-              style={{ borderRadius: 50 }}
-              // transition={{ duration: 3 }}
-            />
-          ) : null}
-        </Box>
-        <Box>
+        <Grid>
+          <Box layoutId="hello" />
+          <Box />
+          <Box />
+          <Box />
+        </Grid>
+        <AnimatePresence>
           {clicked ? (
-            <Circle
-              layoutId="circle"
-              style={{ borderRadius: 0, scale: 2 }}
-              // transition={{ duration: 3 }}
-            />
+            <Overlay
+              initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+              animate={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+              exit={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+            >
+              <Box layoutId="hello" style={{ width: 400, height: 200 }} />
+            </Overlay>
           ) : null}
-        </Box>
+        </AnimatePresence>
       </Wrapper>
     </>
   );
