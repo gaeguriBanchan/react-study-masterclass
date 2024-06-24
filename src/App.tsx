c404d8fd32127cc9a8cb2807426536ba8fb55141
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-06-24 17:30:02 +0900

    Header(useMatch, useAnimation, useScroll, useMotionValueEvent)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import Tv from './routes/Tv';
+import Search from './routes/Search';
+import Header from './components/Header';
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
